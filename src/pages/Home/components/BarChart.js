6a9cf78e5821e6ec20aab6b6c929be66164db029
stonepig <1048218204@qq.com>
2025-02-15 20:42:55 +0800

    chart柱状图组件封装

---
+++ src/pages/Home/components/BarChart.js
@@ -0,0 +1,37 @@
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+
+const BarChart = ({title}) => {
+    const chartRef = useRef(null);
+    useEffect(() => {
+        //放在useEffect中获取dom是为了确保当前dom可用
+        var chartDom = chartRef.current;
+        var myChart = echarts.init(chartDom);
+        var option;
+
+        option = {
+        title: {
+            text:title
+        },
+        xAxis: {
+            type: 'category',
+            data: ['vue', 'angular', 'react']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+            data: [50, 70, 100],
+            type: 'bar'
+            }
+        ]
+        };
+
+        option && myChart.setOption(option);
+    }, [])
+    return <div ref={chartRef} style={{width:"500px", height:"400px"}}></div>
+   
+}
+
+export default BarChart
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,9 @@
-import * as echarts from 'echarts';
-import { useEffect, useRef } from 'react';
 
+import BarChart from './components/BarChart';
 
 const Home = () => {
-    const chartRef = useRef(null);
-    useEffect(() => {
-        //放在useEffect中获取dom是为了确保当前dom可用
-        var chartDom = chartRef.current;
-        var myChart = echarts.init(chartDom);
-        var option;
 
-        option = {
-        xAxis: {
-            type: 'category',
-            data: ['vue', 'angular', 'react']
-        },
-        yAxis: {
-            type: 'value'
-        },
-        series: [
-            {
-            data: [50, 70, 100],
-            type: 'bar'
-            }
-        ]
-        };
-
-        option && myChart.setOption(option);
-    }, [])
-    return <div>
-        <div ref={chartRef} style={{width:"500px", height:"400px"}}></div>
-    </div>
+    return <div><BarChart title={"三大框架使用率"}></BarChart> <BarChart title={"三大框架满意度"}></BarChart> </div>
 }
 
 export default Home;
